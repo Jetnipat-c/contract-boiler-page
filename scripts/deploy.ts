d1429343e37445780e6722a00c3df78356bb3bb9
Jetnipat-c <alonecheer@hotmail.com>
2022-10-23 03:20:37 +0700

    🎉 init: Example

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,9 @@
-import { ethers } from "hardhat";
+import { deployERC20Token } from "./deploy/Token";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = ethers.utils.parseEther("1");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+  await deployERC20Token();
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
